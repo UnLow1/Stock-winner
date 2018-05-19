8dfe334c38df090bb4e7b16a845ca45e0502d856
ezjamad <adam.jamka@ericsson.com>
2018-05-19 22:29:44 +0200

    Added functionality for navigation bar

--- stock_winner/src/main/webapp/src/app/app.module.ts
+++ stock_winner/src/main/webapp/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {RegistrationFormComponent} from "./components/registration-form-component/registration-form.component";
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { LoginFormComponent } from './components/login/login-form/login-form.component';
 import { AuthenticationService } from './services/authentication.service';
-import { MainPageComponent } from './components/main-page/main-page.component';
 import { StartPageComponent } from './components/start-page/start-page.component';
 import {TokenInterceptor} from "./services/token-interceptor";
 
@@ -17,7 +15,6 @@ import {TokenInterceptor} from "./services/token-interceptor";
   declarations: [
     AppComponent,
     LoginFormComponent,
-    MainPageComponent,
     StartPageComponent,
     RegistrationFormComponent,
     AppComponent
