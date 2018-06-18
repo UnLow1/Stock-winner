1157df284574bad3a2d0e2216652a8feee0cf720
Wojciech Chmielarz <wchmielarz@protonmail.com>
2018-06-18 22:56:08 +0200

    new database docker config

--- stock_winner/src/main/docker/docker_init.sh
+++ stock_winner/src/main/docker/docker_init.sh
@@ -2,8 +2,8 @@
 
 check=$( sudo docker ps -a -f "name=stockwinner-postgresql" | tail -n +2 )
 if [ -z "$check" ]; then
-  docker build -t skysurferone/postgres_mypp .
-  docker run --name stockwinner-postgresql -d -p 5432:5432 skysurferone/postgres_mypp
+  docker build -t agh/postgres_stockwinner .
+  docker run --name stockwinner-postgresql -d -p 5432:5432 agh/postgres_stockwinner
 fi
 
 docker start stockwinner-postgresql
