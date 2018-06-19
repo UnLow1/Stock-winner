76deac3e0a234b15e8e2fe03fa7672d81b8d771e
Wojciech Chmielarz <wchmielarz@protonmail.com>
2018-06-19 10:38:31 +0200

    update config

--- stock_winner/src/main/docker/docker_init.sh
+++ stock_winner/src/main/docker/docker_init.sh
@@ -2,8 +2,8 @@
 
 check=$( sudo docker ps -a -f "name=stockwinner-postgresql" | tail -n +2 )
 if [ -z "$check" ]; then
-  docker build -t agh/postgres_stockwinner .
-  docker run --name stockwinner-postgresql -d -p 5432:5432 agh/postgres_stockwinner
+  docker build -t postgres_stockwinner .
+  docker run --name stockwinner-postgresql -d -p 5432:5432 postgres_stockwinner
 fi
 
 docker start stockwinner-postgresql
